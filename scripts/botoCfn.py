5fcd85443cc3edb7ec377187ca7de260a7de2385
John Phelps <jphelps@taos.com>
2013-06-20 20:16:46 +0000

    Latest template

--- scripts/botoCfn.py
+++ scripts/botoCfn.py
@@ -9,14 +9,9 @@ import inspect
 
 cfn = boto.connect_cloudformation()
 
-stacks = []
 #for s in cfn.list_stacks():
     #pprint(pprint(s.__dict__))
 stack = cfn.describe_stacks('PuppetClassuser-01')
-pprint( stack[0] )
-pprint( inspect.getmembers(stack[0], predicate=inspect.ismethod) )
-print stack[0].outputs
-o = {}
 for pair in stack[0].outputs:
     print pair.key
     print pair.value
