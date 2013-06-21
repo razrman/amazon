7ea5246553b025452ca84efba84772760ab232e4
John Phelps <jphelps@taos.com>
2013-06-21 02:25:06 +0000

    Fixed hostname

--- scripts/botoCfn.py
+++ scripts/botoCfn.py
@@ -6,12 +6,52 @@ import time
 import yaml
 from pprint import pprint
 import inspect
+import route53
+from datetime import datetime
+
+def logger(mess):
+    dt = datetime.now()
+    sys.stdout.write(dt.strftime('%Y%m%d %H:%M:%S '))
+    sys.stdout.write(mess)
+    sys.stdout.write('\n')
+    sys.stdout.flush()
+
+def get_zid(r53,zone):
+
+    response = r53.get_all_hosted_zones()
+    for zinfo in response['ListHostedZonesResponse']['HostedZones']:
+        zname = zinfo['Name']
+        zid = zinfo['Id'].split("/")[-1]
+        if zone == zname:
+            return zid
+    #route53.change_record(r53,zid,hostname,'CNAME',rrname,ttl=300)
+
 
 cfn = boto.connect_cloudformation()
 
 #for s in cfn.list_stacks():
     #pprint(pprint(s.__dict__))
-stack = cfn.describe_stacks('PuppetClassuser-01')
-for pair in stack[0].outputs:
-    print pair.key
-    print pair.value
+
+r53 = boto.connect_route53()
+z = 'puppetclass.taoslab.com.'
+zid = get_zid(r53,z)
+logger("Zone: "+z+" Id: "+zid)
+
+for uid in range(25,27):
+    stackname = 'PuppetClassuser-%02d' % uid
+    stack = cfn.describe_stacks(stackname)
+    for pair in stack[0].outputs:
+        logger (stackname + ": " + pair.key + "=" + pair.value)
+        if 'web' in pair.key:
+            host = 'web%02d' % uid
+            fqdn = host + '.puppetclass.taoslab.com.'
+            logger(fqdn+' ('+pair.value+')')
+            route53.add_record(r53,zid,fqdn,'A',pair.value, ttl=300)
+        if 'jump' in pair.key:
+            host = 'jump%02d' % uid
+            fqdn = host + '.puppetclass.taoslab.com.'
+            logger(fqdn+' ('+pair.value+')')
+            route53.add_record(r53,zid,fqdn,'A',pair.value, ttl=300)
+    time.sleep(1)
+
+  
