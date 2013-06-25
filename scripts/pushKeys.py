08fd1935641954527469e0f5f3e47d951fc111e8
John Phelps <jphelps@taos.com>
2013-06-25 22:28:19 +0000

    Fixed pushKeys.py

--- scripts/pushKeys.py
+++ scripts/pushKeys.py
@@ -10,9 +10,14 @@ for uid in range(1,2):
     keyname = "/home/ec2-user/.ssh/puppet%02d.pem" % uid
     jump = "jump"+user+".puppetclass.taoslab.com"
 
-    c1 = "scp -i " + keyname + " " + jump + ":~/.ssh/id_rsa"
+    c1 = "scp -q -i " + keyname + " " + keyname + " " + jump + ":~/.ssh/id_rsa"
     print c1
     system(c1)
+
+    c1 = "ssh -i " + keyname + " " + jump + " 'chmod 600 ~/.ssh/id_rsa'" 
+    print c1
+    system(c1)
+
     for vm in ['web', 'dns', 'puppet', 'db']:
         c1 = "ssh -i " + keyname + " " + jump + " 'scp -o StrictHostKeyChecking=no ~/.ssh/id_rsa " + vm + ":~/.ssh/id_rsa'"
         print c1
